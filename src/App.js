3182f5f5ea992440205c601de142b644a1aad83e
Denis <105867382+SUSocean@users.noreply.github.com>
2023-04-15 18:26:57 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MoviesList from './features/movies/MoviesList'
+
+function App() {
+  return (
+    <>
+      This is goind to be my best web page so far
+      <MoviesList />
+    </>
+  );
+}
+
+export default App;
